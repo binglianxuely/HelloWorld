cb6f6a52039a126eea200dec314a529a28861356
LY <bingxuewangzi@126.com>
2025-06-17 23:04:10 +0800

    实现冒泡排序算法。

--- main.cpp
+++ main.cpp
@@ -2,7 +2,18 @@
 
 void sort(int arr[], int length)
 {
-	
+	for(int i = 0; i < length; i++)
+	{
+		for(int j = 0; (j < length - 1 - i); j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
 }
 
 int main(void)
