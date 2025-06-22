17cdc77214dd28b810e5d590318b720c8938168a
LY <bingxuewangzi@126.com>
2025-06-22 16:46:11 +0800

    修改排序算法：由冒泡排序改为快速排序。

--- main.cpp
+++ main.cpp
@@ -1,33 +1,70 @@
 #include<iostream>
 
-void sort(int arr[], int length)
+void display(int arr[], int length)
 {
-	for(int i = 0; i < length; i++)
+	for (size_t i = 0; i < length; i++)
 	{
-		for(int j = 0; (j < length - 1 - i); j++)
-		{
-			if(arr[j] > arr[j+1])
-			{
-				int tmp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tmp;
-			}
-		}
+		std::cout << arr[i] << " ";
 	}
+	putchar('\n');
+}
+
+void swap(int& x, int& y)
+{
+	int tmp = x;
+	x = y;
+	y = tmp;
+}
+
+int partition(int arr[], int left, int right)
+{
+	int mark = arr[left];
+
+	int start = left, end = right;
+
+	while (left < right)
+	{
+		while (arr[left] <= mark && left < end)
+		{
+			left++;
+		}
+
+		while (arr[right] >= mark && right > start)
+		{
+			--right;
+		}
+
+		if(left < right)
+			swap(arr[left], arr[right]);
+		
+	}
+	
+	swap(arr[start], arr[right]);
+	
+	return right;
+}
+
+void quickSort(int arr[], int left, int right)
+{
+	if (left >= right)
+		return;
+
+	int mid = partition(arr, left, right);
+	
+	quickSort(arr, 0, mid - 1);
+
+	quickSort(arr, mid + 1, right);
 }
 
 int main(void)
 {
-	int arr[] = {10, 68, 93, 52, 86, 42, 23, 16, 8, 36};
-	
+	int arr[] = { 100, 68, 93, 52, 86, 42, 23, 16, 8, 36 };
+
 	int length = sizeof(arr) / sizeof(arr[0]);
-	
-	sort(arr, length);
-	
-	for(const auto& a : arr)
-		std::cout << a << " ";
-	
-	putchar('\n');
-	
+
+	quickSort(arr, 0, length - 1);
+
+	display(arr, length);
+
 	return 0;
 }
